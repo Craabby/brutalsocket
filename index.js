bdf8667653bbceeca2410891677980bf4d6dc7b7
Craabby <69945662+Craabby@users.noreply.github.com>
2021-03-29 06:43:44 -0400

    const > var

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const brutalSocket = require("./brutalsocket");
 
-var bot = new brutalSocket("ws://158.69.123.15:8103/");
+const bot = new brutalSocket("ws://158.69.123.15:8103/");
 bot.on('error') = err => {
   console.error(err);
 };

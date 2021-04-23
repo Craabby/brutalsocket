ad87ea6c5c7f76fea11136b3f4146349f06c501f
Craabby <69945662+Craabby@users.noreply.github.com>
2021-04-22 21:16:32 -0400

    fix embarrassing mistake

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const brutalSocket = require("./brutalsocket");
+const brutalSocket = require("brutalsocket");
 
 const bot = new brutalSocket("ws://158.69.123.15:8103/");
-bot.on('error') = err => {
-  console.error(err);
-};
-bot.on('open') = () => {
-  bot.spawn("sdf");
-};
+bot.on('error', console.err);
+bot.on('open', () => {
+  bot.spawn('crabby')
+})
